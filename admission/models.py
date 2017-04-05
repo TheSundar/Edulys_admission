d6d7c38aa0659d0d1687cf2851dae74f67bbb939
Amancharla Sundaranataraja (uidj8906) <sundaranataraja.amancharla@continental-corporation.com>
2017-04-05 20:41:38 +0530

    FirstCommit
    commit

---
+++ admission/models.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class StudentDetails(models.Model):
+    blood_grp = (('O+','O_Pos'),
+                 ('O-','O_Neg'),
+                 ('B+', 'B_Pos'),
+                 ('B-', 'B_Neg'),
+                 ('A+', 'A_Pos'),
+                 ('A-', 'A_Neg'),
+                 ('AB+', 'AB_Pos'),
+                 ('AB-', 'AB_Neg'))
+    gender = (('Male','Male'),
+              ('Female','Female'),
+              ('Other','Other'))
+    student_first_name = models.CharField(max_length=40)
+    student_middle_name = models.CharField(max_length=40)
+    student_last_name = models.CharField(max_length=40)
+    student_age = models.IntegerField(default=0)
+    student_blood_grp = models.CharField(max_length=3, choices=blood_grp)
+    student_gender = models.CharField(max_length=6, choices=gender)
+    student_admission_to = models.CharField(max_length=100)
+    student_dob = models.DateField()
+
