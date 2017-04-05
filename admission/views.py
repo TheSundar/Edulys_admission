fb86f36c2887b9b9c14a966f8b509cc699d68c93
sundar <iamsundar04@gmail.com>
2017-04-05 20:56:50 +0530

    Basic URL

--- admission/views.py
+++ admission/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 import datetime
 
 # Create your views here.
-def current_datetime(request):
+def save_data(request):
+    name = request.GET.get('name')
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body>It is now %s.</body></html>" % name
     return HttpResponse(html)
