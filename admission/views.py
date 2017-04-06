758b08ad9cb6e91ac8592566d597958244c7f17e
sundar <iamsundar04@gmail.com>
2017-04-06 20:34:35 +0530

    abs

--- admission/views.py
+++ admission/views.py
@@ -7,4 +7,5 @@ def save_data(request):
     name = request.GET.get('name')
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % name
+    
     return HttpResponse(html)
