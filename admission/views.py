c4274a42291db427ade4819bb29d8f73fb1c5499
Cody <cody@quantifiedcode.com>
2017-04-11 15:38:33 +0000

    Migrated `%` string formating

--- admission/views.py
+++ admission/views.py
@@ -5,5 +5,5 @@ import datetime
 # Create your views here.
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body>It is now {0!s}.</body></html>".format(now)
     return HttpResponse(html)
